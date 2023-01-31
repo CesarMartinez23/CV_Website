342d6de1581a1da6c30b64be9a8043926fb89857
MINEDUCYT <lunaacosta>
2023-01-31 00:24:08 -0600

    Changes Added

--- js/main.js
+++ js/main.js
@@ -1,9 +1,13 @@
 const changeColor = document.getElementById("changeColor");
+const section = document.getElementById("ejercicio");
+const colors = ["blue", "pink", "violet", "red", "skyblue", "green", "white"]
 
-changeColor.addEventListener("click", function () {
-  let section = document.getElementById("ejercicio");
-  section.style.backgroundColor = "skyblue";
+changeColor.addEventListener("click", () => {
+  const colorIndex = parseInt(Math.random()*colors.length)
+
+  section.style.backgroundColor = colors[colorIndex]
   section.style.color = "black";
+
   changeColor.style.backgroundColor = "black";
   changeColor.style.color = "white";
 });
